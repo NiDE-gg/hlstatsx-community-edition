39fb25abd78abe76c07a44328277f420454acfbb
Rushaway <rushaway@hotmail.fr>
2026-02-27 18:17:02 +0100

    Improved global chat search functionality
    1) Security updates.
    2) Added the ability to search in chat for text shorter than 4 characters.
    3) Common code moved for reuse in another file.
    
    Co-Authored-By: A1m` <33463136+A1mDev@users.noreply.github.com>

--- web/pages/chat.php
+++ web/pages/chat.php
@@ -287,7 +287,6 @@ class Auth
 
 	$delaySql = "";
 	$delayChat = (int)CHAT_DELAY_TIME;
-	$delayChatSafeHtml = htmlspecialchars($delayChat, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 
 	if ($delayChat > 0 && $delayChat <= CHAT_DELAY_MAX_TIME) {
 		$delayChatSafeSql = $db->escape($delayChat);
@@ -295,7 +294,7 @@ class Auth
 	}
 
 	$serversList = getServersByGame($db, $checkGame);
-	$filter = isset($_REQUEST['filter']) ? $_REQUEST['filter'] : "";
+	$filter = getChatFilterParam();
 
 	// Functions
 	// Old limit 50
@@ -363,13 +362,13 @@ class Auth
 					<?php endforeach; ?>
 				</select>
 
-				Filter: <input type="text" name="filter" value="<?php echo htmlentities($filter); ?>" /> 
+				Filter: <input type="text" name="filter" value="<?=eHtml($filter); ?>" />
 				<input type="submit" value="View" class="smallsubmit" />
 			</form>
 			</span>
 			<?php if (!empty($delaySql)) : ?>
 				<div style="font-size:0.9em; color:#8d90a3; margin-top:10px;">
-					*Messages are delayed by <?=$delayChatSafeHtml;?> minutes to prevent real-time tracking.
+					*Messages are delayed by <?=eHtml($delayChat);?> minutes to prevent real-time tracking.
 				</div>
 			<?php endif; ?>
 		</div>
@@ -462,14 +461,7 @@ class Auth
 					"sortorder"
 				);
 			}
-			$whereclause2='';
-
-			if(!empty($filter))
-			{
-				$whereclause2="AND MATCH (hlstats_Events_Chat.message) AGAINST ('" . $db->escape($filter) . "' in BOOLEAN MODE)";
-			}
-
-			$surl = $g_options['scripturl'];
+			$whereclause2 = buildSearchSqlSafe($db, $filter);
 
 			$result = $db->query
 			("
